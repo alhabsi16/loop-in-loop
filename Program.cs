bc25fb964c41c2b9884ee4097d96b73d9031124d
alhabsi16 <mxmx8283@gmail.com>
2023-06-05 09:09:54 +0400

    print ***

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             To Print
+            *
+            **
+            ***
+            ****
+            *****
+            ******
+            
+
+             */
+
+            for (int i = 0; i <= 6 ; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    
+                 
+                        Console.Write("*");
+                   
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 6; i >= 1; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+                
+            }
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("*");
+            }
+           
+            }
+    }
+}
